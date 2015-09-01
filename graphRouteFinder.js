36d132aa760368f707ad7aecb62758a055fc18bb
Arya Solhi <aryasolhi@gmail.com>
2015-09-01 18:28:17 -0400

    Finished directed route checker.  Uses depth-first search.  Forgets visited array along breadth.  Runtime on supported cases should be O(n^2) because of how it checks if a node has already been traversed.

--- graphRouteFinder.js
+++ graphRouteFinder.js
@@ -1,13 +1,43 @@
-// Given a directed graph, design an algorithm to find out whether there is a route be- tween two nodes.
+// Given a directed graph, design an algorithm to find out whether there is a route between two nodes.
 
 (function(){
     'use strict';
 
     var samplegraph = {
-        "vectors":{
-        },
-        "edges":{
+            "A":["B"],
+            "B":["C"],
+            "C":["D"],
+            "D":["F"],
+            "E":["F"],
+            "F":["A"]
+        };
+    
+    function notchecked(item, visited){
+        for (var i = 0; i<visited.length; i++){
+            if (item === visited[i]){
+                return false;
+            }
         }
-    };
+        return true;
+    }
+    
+    function doesRouteExist (graph, origin, dest){
+        var visited = (arguments.length === 4)?arguments[3]:[];
+        console.log(visited);
+        var len = graph[origin].length;
+        visited.push(origin);
+        for (var i = 0; i<len; i++){
+            console.log(i + ". origin: "+origin+"\n");
+            if (graph[origin][i] === dest){
+                return true;
+            }else if (notchecked(graph[origin][i], visited) && doesRouteExist(graph, graph[origin][i], dest, visited)){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    console.log((doesRouteExist(samplegraph, "A", "E"))?"super possible":"less possible");
+    
     
 })();
